549cc00fe01deeb32ae57b4875d1cdc7e8f28faa
antosha-85 <antosha-85@yandex.ru>
2020-02-06 23:31:45 +0000

    finished lighthouseBnB add property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -131,7 +131,7 @@ const getAllProperties = function(options, limit = 10) {
   var where = `WHERE`;
   // 3
   if (options.owner_id) {
-    queryParams.push('${options.owner_id}');
+    queryParams.push(`${options.owner_id}`);
     queryString += `${where} owner_id = $${queryParams.length}`;
     where = `AND`;
   }
@@ -166,7 +166,7 @@ const getAllProperties = function(options, limit = 10) {
     where = `AND`;
   }
   queryParams.push(limit);
-  queryString += `ORDER BY cost_per_night
+  queryString += ` ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
 
@@ -190,9 +190,14 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `INSERT INTO properties 
+    (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms ) 
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`;
+  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
+  return pool.query(queryString, values)
+  .then(res => {
+  console.log("TCL: addProperty -> res", res.rows)
+      return res.rows[0];
+  });
 }
 exports.addProperty = addProperty;
